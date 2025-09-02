7037a326e9240982458c7e97d25babe10fefc32a
zaixrx <zaizroff@gmail.com>
2025-09-02 16:35:49 +0100

    the grapher can draw 1D graphs

--- examples/grapher.c
+++ examples/grapher.c
@@ -1,3 +1,4 @@
+#include <math.h>
 #include <raylib.h>
 #include <raymath.h>
 
@@ -5,43 +6,74 @@
 #define WIN_H 600
 #define FPS 60
 
+#define UNIT_SIZE 100
+
+typedef float(*Func1D)(float);
+
 int IntegerPart(float x) {
 	if (x >= 0) return (int)x;
 	return (int)x-1;
 }
 
-void DrawCartesianGraph(Camera2D camera, int unit_size, float thickness, Color color) {
+Vector2 GetCamOffset(Camera2D camera) {
+	return (Vector2){
+		.x = IntegerPart(camera.target.x/UNIT_SIZE)*UNIT_SIZE,
+		.y = IntegerPart(camera.target.y/UNIT_SIZE)*UNIT_SIZE,
+	};
+}
+
+void DrawCartesianGraph(Camera2D camera, float thick, Color color) {
 	DrawLineEx(
 		(Vector2){ WIN_W/2.0F, camera.target.y },
 		(Vector2){ WIN_W/2.0F, camera.target.y+WIN_H },
-		2.0F,
+		thick,
 		color
 	);
 	DrawLineEx(
 		(Vector2){ camera.target.x, WIN_H/2.0F },
 		(Vector2){ camera.target.x+WIN_W, WIN_H/2.0F },
-		2.0F,
+		thick,
 		color
 	);
 
-	color.a /= 4;
-	float left_most = IntegerPart(camera.target.x/unit_size)*unit_size;
-	float top_most  = IntegerPart(camera.target.y/unit_size)*unit_size;
-	for (int x = 0; x <= WIN_W/unit_size; ++x) {
+	color.a /= 10;
+	Vector2 offset = GetCamOffset(camera);
+	for (int x = 0; x <= WIN_W/UNIT_SIZE; ++x) {
 		DrawLineEx(
-			(Vector2){ left_most+unit_size*x, camera.target.y },
-			(Vector2){ left_most+unit_size*x, camera.target.y+WIN_H },
-			2.0F,
+			(Vector2){ offset.x+UNIT_SIZE*x, camera.target.y       },
+			(Vector2){ offset.x+UNIT_SIZE*x, camera.target.y+WIN_H },
+			thick,
 			color
 		);
 	}
-	for (int y = 0; y <= WIN_H/unit_size; ++y) {
+	for (int y = 0; y <= WIN_H/UNIT_SIZE; ++y) {
 		DrawLineEx(
-			(Vector2){ camera.target.x, top_most+unit_size*y },
-			(Vector2){ camera.target.x+WIN_W, top_most+unit_size*y },
-			2.0F,
+			(Vector2){ camera.target.x      , offset.y+UNIT_SIZE*y },
+			(Vector2){ camera.target.x+WIN_W, offset.y+UNIT_SIZE*y },
+			thick,
 			color
 		);
+	}
+}
+
+static inline float sign(float x) {
+	return x >= 0 ? 1 : -1;
+}
+
+static inline float ToCartesianX(float x) {
+	return (x-(WIN_W>>1))/UNIT_SIZE;
+}
+
+static inline float ToWorldY(float y) {
+	return WIN_H-(y*UNIT_SIZE+(WIN_H>>1));
+}
+
+void GraphFunc1D(Func1D fn, float step, Camera2D camera, float thick, Color color) {
+	float win2 = WIN_W/2.0F;
+	for (float x = camera.target.x; x <= camera.target.x+WIN_W; x += step) {
+		Vector2 start = { x, ToWorldY(fn(ToCartesianX(x))) };
+		Vector2 end   = { x+step, ToWorldY(fn(ToCartesianX(x+step))) };
+		DrawLineEx(start, end, thick, color);
 	}
 }
 
@@ -68,7 +100,8 @@ int main(void) {
 		BeginDrawing();
         		ClearBackground(BLACK);
 			BeginMode2D(camera);
-			DrawCartesianGraph(camera, 100.0F, 25.0F, WHITE);
+			DrawCartesianGraph(camera, 2.0F, WHITE);
+			GraphFunc1D(expf, 0.1F, camera, 2.0F, BLUE);
 			EndMode2D();
         	EndDrawing();
     	}
